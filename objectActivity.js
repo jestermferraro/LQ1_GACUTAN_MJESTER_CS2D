cb98bae29b2c7f6e0fa48c5b41319168065c1e5e
jestermferraro <jestermferraro@gmail.com>
2024-10-16 23:03:04 +0800

    Object Activity

---
+++ objectActivity.js
@@ -0,0 +1,11 @@
+let car = {
+    type: "Koenigsegg",
+    model: "Jesko Absolut",
+    color: "Exposed Carbon Black"
+};
+
+console.log(car.type);
+car.type = "Toyota";
+console.log(car);
+car.wheels = 4;
+console.log(car);
