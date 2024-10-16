538652678f3f47745bf6be78de78b0385e8936bd
jestermferraro <jestermferraro@gmail.com>
2024-10-16 22:53:41 +0800

    Array Activity

---
+++ arrayActivity.js
@@ -0,0 +1,11 @@
+let favSingers = ["Drake", "Aftertheparty", "21 Savage"];
+
+console.log(favSingers[0]);
+let favNumbers = [28, 99, 89, 7];
+let mixedArr = ["string", ["other array"], 123, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
+console.log(mixedArr[1][0]);
